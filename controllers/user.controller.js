9a205b74b87edff5cc47682092dea4e2c3c30717
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-01-27 17:13:35 +0200

    User CRUD done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,17 +10,69 @@ module.exports.createUser = async (req, res, next) => {
     }
 }
 module.exports.getAllUsers = async (req, res, next) => {
-    //TODO
+    try {
+        const allUsers = await User.findall();
+        res.status(200).send(allUsers)
+    } catch(error) {
+        next(error)
+    } 
 }
 
 
 
 
 module.exports.getOneUser = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const user = await User.findByPk(userId);
+        res.status(200).send(user);
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports.updateUser = async (req, res, next) => {
+    try {
+        const {body, params: {userId}} = req;
+        const [rowCount, [updated]] = await User.update(body, {
+            where: {
+                id: userId
+            },
+            returning: true
+        });
+        res.status(200).send(updated);
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports.deleteUser = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const deletedUser = await User.destroy({
+            where: {
+                id: userId
+            },
+            returning: true
+        })
+        if (deletedUser) {
+            res.status(200).send({data: deletedUser})
+        } else {
+            res.status(404)
+        }
+    }catch(error) {
+        next(error)
+    }
+}
+
+module.exports.deleteInstance = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const user = await User.findByPk(userId);
+        if (user) {
+        const result = await user.destroy();
+        }
+    } catch (error) {
+        next(error);
+    }
 }
