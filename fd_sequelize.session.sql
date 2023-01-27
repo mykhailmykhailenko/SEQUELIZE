538c54c521ba686daa3fa1e0a9425f572aaba60b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-01-27 11:46:13 +0200

    Sequelize signle model setup

---
+++ fd_sequelize.session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    first_name varchar(200) NOT NULL,
+    last_name varchar(200) NOT NULL,
+    email varchar(200) UNIQUE NOT NULL
+);
+
+DELETE TABLE users;
